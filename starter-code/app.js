a16f9b1708d7e34d45dcff0d2ce54c4c82ad60a7
Steve Ngo <stevehngo@hotmail.com>
2021-08-14 07:32:18 -0400

    update problem 4

--- starter-code/app.js
+++ starter-code/app.js
@@ -87,12 +87,16 @@ Test this function by hand in the console to get it working, and when you think 
 let testArray = [2, 3, 4]; //eslint-disable-line
 
 function sumArray(sumArr) { //eslint-disable-line
+let arrayTotal = sumAndMultiply(testArray[0], testArray[1], testArray[2], );
 
+let sumArrayString = testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + arrayTotal[0] + ' is their sum.';
+
+return [arrayTotal[0], sumArrayString]
 }
 
 // Here is the test for sumArray(); uncomment it to run it
 
-// testSumArray(testArray);
+testSumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
