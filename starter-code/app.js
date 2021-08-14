bc2942c145c8e8e9b6fefccab9c50dcd0b6eea07
Steve Ngo <stevehngo@hotmail.com>
2021-08-14 06:37:22 -0400

    update problem 3

--- starter-code/app.js
+++ starter-code/app.js
@@ -55,10 +55,21 @@ Test this function by hand in the console to get it working, and when you think 
 function sumAndMultiply(a, b, c) { //eslint-disable-line
 // you get back an array from sum!!!
 // make sure your get the correct index number to get the number from sum
+let sumTotal = sum(a, b); 
+sumTotal = sum(sumTotal[0], c);
+
+let productTotal = multiply(a,b);
+productTotal = multiply(productTotal[0], c);
+
+let sumString = a + ' and ' + b + ' and ' + c + ' sum to ' + sumTotal[0] + '.';
+let productString = 'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + productTotal[0] + '.';
+
+return [sumTotal[0], productTotal[0], sumString, productString];
+
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
